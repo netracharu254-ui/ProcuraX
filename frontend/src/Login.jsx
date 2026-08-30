4cb7b53cf948d92b38867dd954d66f28adc30105
Charu Netra <netracharu254@gamil.com>
2026-08-30 20:52:02 +0530

    Fix Render HTTPS backend URL

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 
-const API = "http://https://procurax-o4mh.onrender.com/api";
-
+const API = "https://procurax-o4mh.onrender.com"
 function Login({ onLogin }) {
   const [isRegister, setIsRegister] = useState(false);
 
